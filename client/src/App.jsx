c9f090d67b9e7cd1eb7d7bc5721b88479e4384d1
Ayyash <ayyashahmedanhardeen@gmail.com>
2024-10-05 13:48:21 -0400

    connected the backend to react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import react, { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,6 +9,10 @@ function App() {
   return (
     <>
       <div>
+        <img 
+        src = "http://100.101.55.122:8000/video_feed"
+        alt = "video"
+        />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -32,4 +36,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
